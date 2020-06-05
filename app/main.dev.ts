4faed8f98464afc695fd8e3b71829b5a9fe745a9
郑温剑 <904656778@qq.com>
2020-06-05 10:57:06 +0800

    版本号更新

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -10,17 +10,9 @@
  */
 import path from 'path';
 import { app, BrowserWindow, ipcMain } from 'electron';
-import { autoUpdater } from 'electron-updater';
-import log from 'electron-log';
+// import { autoUpdater } from 'electron-updater';
+// import log from 'electron-log';
 // import MenuBuilder from './menu';
-
-export default class AppUpdater {
-  constructor() {
-    log.transports.file.level = 'info';
-    autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
-  }
-}
 
 let mainWindow: BrowserWindow | null = null;
 
@@ -94,7 +86,9 @@ const createWindow = async () => {
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
-  new AppUpdater();
+  // new AppUpdater();
+
+  require('update-electron-app')();
 };
 
 /**
